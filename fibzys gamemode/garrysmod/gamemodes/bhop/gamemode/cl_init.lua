dd1be68fccd1b3b9418c56f4e7025d30d322d770
FiBzY <jwolf2110@gmail.com>
2025-03-27 18:24:08 -0400

    Add playerclass for map spawn points

--- fibzys gamemode/garrysmod/gamemodes/bhop/gamemode/cl_init.lua
+++ fibzys gamemode/garrysmod/gamemodes/bhop/gamemode/cl_init.lua
@@ -8,6 +8,7 @@ local files = {
     shared = {
         "essential/sh_config.lua",
         "shared.lua",
+        "sh_playerclass.lua",
         "essential/sh_movement.lua",
         "essential/sh_network.lua",
         "essential/sh_utilities.lua",
